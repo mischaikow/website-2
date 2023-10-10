8f5953584ed461f4e9e5c6f7fdb0806b4fab587b
mischaikow <mischaik@gmail.com>
2023-10-10 16:59:56 -0400

    fix typos and add a pause

--- build_run_docker.sh
+++ build_run_docker.sh
@@ -1,14 +1,15 @@
 cd /home/ec2-user/website
 
 git pull
-dt = $(date '+%d/%m/%Y %H:%M:%S')
+dt=$(date '+%d/%m/%Y %H:%M:%S')
 echo 'git pulled' $dt 
 
 cd backend
 docker buildx build --no-cache -t site/backend -f Dockerfile .
 docker stop mischaikow-backend
 docker run --rm -d -p 5000:5000 --name mischaikow-backend --init site/backend
-dt = $(date '+%d/%m/%Y %H:%M:%S')
+dt=$(date '+%d/%m/%Y %H:%M:%S')
 echo 'Script Completed' $dt 
 
+unset dt
 curl http://127.0.0.1:5000
