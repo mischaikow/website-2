0c8731c4c100d669f541452e85b55b62829ce57d
mischaikow <mischaik@gmail.com>
2023-10-10 16:50:21 -0400

    new command executions

--- build_run_docker.sh
+++ build_run_docker.sh
@@ -1,7 +1,14 @@
 cd /home/ec2-user/website
+
 git pull
-npm install
+dt = $(date '+%d/%m/%Y %H:%M:%S')
+echo 'git pulled' $dt 
+
 cd backend
 docker buildx build --no-cache -t site/backend -f Dockerfile .
 docker stop mischaikow-backend
 docker run --rm -d -p 5000:5000 --name mischaikow-backend --init site/backend
+dt = $(date '+%d/%m/%Y %H:%M:%S')
+echo 'Script Completed' $dt 
+
+curl http://127.0.0.1:5000
