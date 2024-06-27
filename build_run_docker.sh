9a12da962cf259c73dfb2ca53b0e40a5846c38a8
mischaikow <mischaik@gmail.com>
2024-06-27 15:13:21 -0400

    fixing docker script

--- build_run_docker.sh
+++ build_run_docker.sh
@@ -5,7 +5,7 @@ git pull
 dt=$(date '+%d/%m/%Y %H:%M:%S')
 echo 'git pull at' $dt 
 
-dcoker network create mischaikow-home
+docker network create mischaikow-home
 
 echo 'Building and deploying backend'
 cd /home/ec2-user/website/backend
@@ -13,7 +13,7 @@ docker buildx build --no-cache -t site/backend -f Dockerfile .
 docker stop mischaikow-backend
 docker run --rm -d -p 5000:5000 --network=mischaikow-home --name mischaikow-backend --init site/backend
 
-sleep 5s
+sleep 10s
 backend=curl http://127.0.0.1:5000
 dt=$(date '+%d/%m/%Y %H:%M:%S')
 if [ $backend == 'Backend is up']
@@ -29,7 +29,7 @@ docker buildx build --no-cache -t site/frontend -f Dockerfile .
 docker stop mischaikow-frontend
 docker run --rm -d -p 4173:4173 --network=mischaikow-home --name mischaikow-frontend --init site/frontend
 
-sleep 5s
+sleep 10s
 frontend=$(curl -o /dev/null -s -w "%{http_code}\n" -I http://127.0.0.1:4173)
 dt=$(date '+%d/%m/%Y %H:%M:%S')
 if [ "$frontend" -eq 200 ]
@@ -45,7 +45,7 @@ docker buildx build --no-cache -t site/nginx -f Dockerfile .
 docker stop mischaikow-nginx
 docker run --rm -d -p 80:80 -p 443:443 --network=mischaikow-home --name mischaikow-nginx --init site/nginx
 
-sleep 5s
+sleep 10s
 dt=$(date '+%d/%m/%Y %H:%M:%S')
 if [ "$( docker container inspect -f '{{.State.Status}}' mischaikow-nginx)" = "running" ];
 then
